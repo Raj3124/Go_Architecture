0f9137fd100ecd512c53f77f4173f45ab2fe0b63
Raj Mahajan <rajmj3113@gmail.com>
2018-03-15 00:03:36 +0530

    added error handling

--- api/api.go
+++ api/api.go
@@ -4,44 +4,41 @@ import(
 	"net"
 	"fmt"
 	"net/http"
-	DB "myarch/sql"
-	resp "myarch/response"
-	logs "myarch/logs"
+	"encoding/json"
+	DB "Go_Architecture/sql"
+	resp "Go_Architecture/response"
+	logs "Go_Architecture/logs"
+	reqs "Go_Architecture/request"
 )
 
 func ErrorMessage(w http.ResponseWriter,r *http.Request){
-
 	resp.Sendresponse(w, r.Body)
 }
 
 func Insert(w http.ResponseWriter,r *http.Request){
 	if(r.Method == "POST"){
+		// fmt.Println()
+		decoder := json.NewDecoder(r.Body)
+		var t reqs.InsertStruct
+		err := decoder.Decode(&t)
+		if err != nil {
+			fmt.Println("error in decoding")
+		}
+		defer r.Body.Close()
+		if t.Test == "" {
+		fmt.Println("empty")
+		}else{
+			fmt.Println(t.Test)
+			DB.Insert(t.Test)
+		}
+		}else{
+			ip, port, err2 := net.SplitHostPort(r.RemoteAddr)
+			if err2 != nil{
+				w.Write([]byte("Cannot get IP"))
+			}
+			userIP := net.ParseIP(ip)
+			logs.Create_log(port,userIP)
+			resp.InvalidMethod(w, r.Body)
+		}
 
-	fmt.Fprintln(w,"Inserting Data"+r.Method)
-	DB.Insert()
-}else{
-	ip, port, err2 := net.SplitHostPort(r.RemoteAddr)
-	if err2 != nil{
-		w.Write([]byte("Cannot get IP"))
-	}
-	userIP := net.ParseIP(ip)
-	logs.Create_log(port,userIP)
-	resp.InvalidMethod(w, r.Body)
-}
-
-}
-
-func Delete(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintln(w,"Deleting data"+r.Method)
-	DB.Delete()
-}
-
-func Update(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintln(w,"Updating data"+r.Method)
-	DB.Update()
-}
-
-func Create(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintln(w,"Creating new Function"+r.Method)
-	DB.Create()
-}
+		}
