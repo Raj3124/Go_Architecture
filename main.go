2d229268d5a7530b430c08c5f5cc62311882dc66
Raj3124 <rajmj3113@gmail.com>
2018-03-19 17:21:12 +0530

    minor modifications

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main(){
 	DB.Start_Con()
 	http.HandleFunc("/",API.ErrorMessage)
 	http.HandleFunc("/insert",API.Insert)
-	err:=http.ListenAndServe(":2011",nil)
+	err:=http.ListenAndServe(":2005",nil)
 	if err!=nil{
 		fmt.Println("Cannot Start Server... check IP and port number")
 	}
