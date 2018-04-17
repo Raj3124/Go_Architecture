b9341dd0ab5abb72eb3a802d2f61b751f4c796ef
Raj3124 <rajmj3113@gmail.com>
2018-04-17 23:07:02 +0530

    Update api.go

--- api/api.go
+++ api/api.go
@@ -41,3 +41,45 @@ func Insert(w http.ResponseWriter,r *http.Request){
 		}
 
 		}
+func View_Attendance(w http.ResponseWriter,r *http.Request){
+    if(r.Method == "GET"){
+   	 // fmt.Println()
+           sem :=  r.URL.Query()["semester"]
+           sub := r.URL.Query()["subject"]
+
+   	 decoder := json.NewDecoder(r.Body)
+   	 var t reqs.View_Attendance
+   	 err := decoder.Decode(&t)
+   	 if err != nil {
+   		 fmt.Println("error in decoding")
+   	 }
+   	 defer r.Body.Close()
+   	 if t.Sem == "" || t.Sub=="" {
+		ip, port, err2 := net.SplitHostPort(r.RemoteAddr)
+		if err2 != nil{
+			w.Write([]byte("Cannot get IP"))
+		}
+		userIP := net.ParseIP(ip)
+		logs.RequestInvalid(port,userIP)
+		resp.InvalidRequestData(w, r.Body)
+   	 }else{
+		ip, port, err2 := net.SplitHostPort(r.RemoteAddr)
+		if err2 != nil{
+			w.Write([]byte("Cannot get IP"))
+		}
+		userIP := net.ParseIP(ip)
+			logs.Successfulapicall(port,userIP)
+   		 DB.View(sem,sub, w)
+   	 }
+   	 }else{
+   		 ip, port, err2 := net.SplitHostPort(r.RemoteAddr)
+   		 if err2 != nil{
+   			 w.Write([]byte("Cannot get IP"))
+   		 }
+   		 userIP := net.ParseIP(ip)
+   		 logs.Create_log(port,userIP)
+   		 resp.InvalidMethod(w, r.Body)
+   	 }
+
+   	 }
+
